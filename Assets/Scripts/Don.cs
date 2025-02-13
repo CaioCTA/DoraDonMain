28074116f580cb99a7d7d28ba9fca139517672df
Caio Ataide <98366011+CaioCTA@users.noreply.github.com>
2025-02-12 21:20:39 -0300

    Don animacoes

--- Assets/Scripts/Don.cs
+++ Assets/Scripts/Don.cs
@@ -356,6 +356,12 @@ public class Don : MonoBehaviour, IPunObservable
     
     //Nado
     private bool _isSwimming;
+    [SerializeField] private float _waterSpeed = 4f;
+    private float _normalGravityScale = 1f; // Gravidade normal fora da água
+    private float _waterGravityScale = 7f; // Gravidade reduzida dentro da água
+    private float _gravityTransitionSpeed = 6f; // Velocidade da transição de gravidade
+    [SerializeField] private float _jumpOutWater = 7.5f;
+    private float _enterWaterSpeedReduction = 5f; // Fator de redução de velocidade ao entrar na água
     
     
     private Rigidbody2D _rb;
@@ -369,6 +375,9 @@ public class Don : MonoBehaviour, IPunObservable
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+        
+        
         lastUpdate = Time.time;
         latestRotation = transform.rotation;
         latestPosition = transform.position;
@@ -398,16 +407,58 @@ public class Don : MonoBehaviour, IPunObservable
 
     void MovePlayer()
     {
-        float moveX = Input.GetAxis("Horizontal") * _movSpeed * Time.deltaTime;
-        
-        _rb.velocity = new Vector2(moveX, _rb.velocity.y);
-        
-        //Pulo
-        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        if (GetComponent<PhotonView>())
         {
-            isGrounded = false;
-            _rb.velocity = new Vector2(_rb.velocity.x, 0); // Zera a velocidade vertical antes de pular
-            _rb.AddForce(new Vector2(0f, _jumpForce), ForceMode2D.Impulse);
+            float moveH = Input.GetAxis("Horizontal");
+
+            if (!_isSwimming)
+            {
+                // Movimento horizontal no chão
+                _rb.velocity = new Vector2(moveH * _movSpeed * Time.deltaTime, _rb.velocity.y);
+                _anim.SetBool("isWalking", true);
+                
+                //Verifica se o Jogador esta pulando
+                if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+                {
+                    _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce * Time.deltaTime);
+                    _anim.SetBool("isJumping", true);
+                }
+                
+            }
+            else
+            {
+                float moveX = Input.GetAxis("Horizontal");
+                float moveY = Input.GetAxis("Vertical");
+                _rb.velocity = new Vector2(moveX * _waterSpeed, moveY * _waterSpeed) * Time.deltaTime;
+
+                if (moveX > 0)
+                {
+                    transform.localScale = new Vector3(2.951194f, 2.951194f, 2.951194f); // Olha para a direita
+                }
+                else if (moveX < 0)
+                {
+                    transform.localScale = new Vector3(-2.951194f, 2.951194f, 2.951194f); // Olha para a esquerda
+                }
+            }
+            
+            bool isWalking = Mathf.Abs(moveH) > 0 && !_isSwimming; // Verifica se o jogador está andando fora do voo
+            _anim.SetBool("isWalking", isWalking); // Atualiza o parâmetro "isWalking" no Animator
+            
+            _anim.SetBool("isSwimming", _isSwimming);
+            
+            //Inverte a direção do sprite com base na direção do movimento
+            if (moveH > 0)
+            {
+                transform.localScale = new Vector3(2.951194f, 2.951194f, 2.951194f); // Olha para a direita
+            }
+            else if (moveH < 0)
+            {
+                transform.localScale = new Vector3(-2.951194f, 2.951194f, 2.951194f); // Olha para a esquerda
+            }
+
+            float targetGravityScale = _isSwimming ? _waterGravityScale : _normalGravityScale;
+            _rb.gravityScale = Mathf.Lerp(_rb.gravityScale, targetGravityScale, _gravityTransitionSpeed);
+            
         }
     }
     
@@ -417,6 +468,11 @@ public class Don : MonoBehaviour, IPunObservable
         {
             transform.position = Vector2.Lerp(transform.position, latestPosition, Time.deltaTime * 5f);
         }
+
+        if (latestRotation != null)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, latestRotation, Time.deltaTime * 5f);
+        }
         
     }
     
@@ -425,6 +481,7 @@ public class Don : MonoBehaviour, IPunObservable
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+            _anim.SetBool("isJumping", false);
         }
     }
     
@@ -436,16 +493,58 @@ public class Don : MonoBehaviour, IPunObservable
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+     {
+         if (other.CompareTag("Water"))
+         {
+             _isSwimming = true;
+             // StartCoroutine(ChangeCollider());
+             // Reduz a velocidade vertical ao entrar na água para evitar impacto
+             _rb.velocity = new Vector2(_rb.velocity.x, _rb.velocity.y * _enterWaterSpeedReduction);
+             // Aplica uma pequena força para cima para simular flutuação inicial
+             _rb.AddForce(Vector2.up * 2f, ForceMode2D.Impulse);
+
+             Debug.Log("To na agua");
+
+         }
+     }
+
+     private void OnTriggerExit2D(Collider2D other)
+     {
+         if (other.CompareTag("Water"))
+         {
+
+             _isSwimming = false;
+             _rb.velocity = new Vector2(_rb.velocity.x, _jumpOutWater);
+             // StartCoroutine(ChangeColliderOut());
+
+
+             Debug.Log("Sai da agua");
+
+
+         }
+     }
+
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.IsWriting)
         {
             stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+            stream.SendNext(transform.localScale.x > 0 ? 1 : -1);
         }
         else
         {
             latestPosition = (Vector3)stream.ReceiveNext();
-            
+            latestRotation = (Quaternion)stream.ReceiveNext();
+            int direction = (int)stream.ReceiveNext();
+
+            transform.localScale = new Vector3(
+                direction * Mathf.Abs(transform.localScale.x),
+                transform.localScale.y,
+                transform.localScale.z);
+             
+             
             //Calcula o tempo desde a ultima atualizacao
             lastUpdate = Time.time;
         }
